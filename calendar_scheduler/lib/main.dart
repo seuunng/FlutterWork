9859a25f016cb76e88b3cba5b5d7b75bb29de792
seuunng <155802305+seuunng@users.noreply.github.com>
2024-09-05 16:38:48 +0900

    재배포 준비

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -1,38 +1,49 @@
-import 'package:calendar_scheduler/provider/schedule_provider.dart';
-import 'package:calendar_scheduler/repository/schedule_repository.dart';
-import 'package:calendar_scheduler/screen/home_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'firebase_options.dart';
-import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:calendar_scheduler/database/drift_database.dart';
-import 'package:provider/provider.dart';
+  import 'dart:io';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  import 'package:calendar_scheduler/provider/schedule_provider.dart';
+  import 'package:calendar_scheduler/repository/schedule_repository.dart';
+  import 'package:calendar_scheduler/screen/home_screen.dart';
+  import 'package:firebase_core/firebase_core.dart';
+  import 'package:google_mobile_ads/google_mobile_ads.dart';
+  import 'firebase_options.dart';
+  import 'package:flutter/material.dart';
+  import 'package:get_it/get_it.dart';
+  import 'package:intl/date_symbol_data_local.dart';
+  import 'package:calendar_scheduler/database/drift_database.dart';
+  import 'package:provider/provider.dart';
 
-  MobileAds.instance.initialize();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  await initializeDateFormatting();
+    // // 플랫폼에 따라 조건부로 ffi 초기화 실행
+    // if (Platform.isAndroid || Platform.isIOS) {
+    //   // 네이티브 플랫폼에서만 실행되는 코드
+    //   initFfi();
+    // } else {
+    //   // 웹에서는 이 코드를 실행하지 않음
+    //   print("Web environment: skipping ffi initialization.");
+    // }
 
-  // final database = LocalDatabase();
-  //
-  // GetIt.I.registerSingleton<LocalDatabase>(database);
-  //
-  // final repository = ScheduleRepository();
-  // final scheduleProvider = ScheduleProvider(repository: repository);
-  runApp(
-      // ChangeNotifierProvider(
-      //   create: (_) => scheduleProvider,
-      //   child: MaterialApp(
-      //     home: HomeScreen(),
-      //   ),
-      // )
-      MaterialApp(debugShowCheckedModeBanner: false,
-          home: HomeScreen()));
-}
+    MobileAds.instance.initialize();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    await initializeDateFormatting();
+
+    // final database = LocalDatabase();
+    //
+    // GetIt.I.registerSingleton<LocalDatabase>(database);
+    //
+    // final repository = ScheduleRepository();
+    // final scheduleProvider = ScheduleProvider(repository: repository);
+    runApp(
+        // ChangeNotifierProvider(
+        //   create: (_) => scheduleProvider,
+        //   child: MaterialApp(
+        //     home: HomeScreen(),
+        //   ),
+        // )
+        MaterialApp(debugShowCheckedModeBanner: false,
+            home: HomeScreen()));
+  }
